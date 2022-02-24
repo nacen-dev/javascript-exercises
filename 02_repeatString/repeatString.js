c683ed1f9e2f7c907a5a3a745ab7202e99172d06
nacen.dev <nacen.dev@gmail.com>
2022-02-24 22:47:52 +0800

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, timesToRepeat) {
+  if (timesToRepeat < 0) return "ERROR";
+  let repeatedString = "";
 
+  for (let i = 0; i < timesToRepeat; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
