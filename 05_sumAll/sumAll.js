d76adab008f964d88f053d4b6e6b01258716aec9
nacen.dev <nacen.dev@gmail.com>
2022-02-25 15:33:21 +0800

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  if (firstNum < 0 || secondNum < 0 || !isNumType(firstNum) || !isNumType(secondNum)) return "ERROR";
+  let sum = 0;
+  let min = Math.min(firstNum, secondNum);
+  let max = Math.max(firstNum, secondNum);
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+const isNumType = (arg) => typeof arg === "number";
+
+console.log(sumAll(5,5))
 
 // Do not edit below this line
 module.exports = sumAll;
