7b440befa9f8e9f02cf73a011da575f8c0b8f6da
nacen.dev <nacen.dev@gmail.com>
2022-02-27 02:09:09 +0800

    Complete 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, currentNum) => sum + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((product, currentNum) => product * currentNum, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let powerNum = base;
+  for (let i = 1; i < exponent; i++) {
+    powerNum *= base;
+  }
+  return powerNum;
+  // return Math.pow(base, exponent); another solution
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  if (num === 0) return result;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
