f0c4190ecb86e5e2a8f4daec0f14f57cfb58cfbe
nacen.dev <nacen.dev@gmail.com>
2022-02-28 21:32:12 +0800

    Complete 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((person, currentPerson) => {
+    let personAge = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : new Date().getFullYear() - person.yearOfBirth;
+    let currentPersonAge = currentPerson.yearOfDeath
+      ? currentPerson.yearOfDeath - currentPerson.yearOfBirth
+      : new Date().getFullYear() - currentPerson.yearOfBirth;
+    if (personAge < currentPersonAge) return currentPerson;
+    return person;
+  });
 };
 
 // Do not edit below this line
