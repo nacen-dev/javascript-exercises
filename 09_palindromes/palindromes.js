56242895330ccbb6458ee006cdb2d04e709dc25a
nacen.dev <nacen.dev@gmail.com>
2022-02-28 20:39:32 +0800

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let word = string.match(/\w/g).join("").toLowerCase();
+  for (
+    let start = 0, end = word.length - 1;
+    start < Math.floor(word.length / 2);
+    start++, end--
+  ) {
+    if (word[start] !== word[end]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
