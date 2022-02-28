9948035a19db257a7fbf313390ca07c6e82d07df
nacen.dev <nacen.dev@gmail.com>
2022-02-28 21:01:38 +0800

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+// Iterative Solution
+const fibonacci = (number) => {
+  let num = Number(number);
+  if (num < 0) return "OOPS";
+  if (num <= 2) return 1;
+  let previousNum = 1;
+  let currentNum = 1;
+  for (let i = 2, temp = 0; i < num; i++) {
+    temp = currentNum;
+    currentNum += previousNum;
+    previousNum = temp;
+  }
+  return currentNum;
 };
+
+// Recursive Solution
+// const fibonacci = (number) => {
+//   if (number < 0) return "OOPS";
+//   if (number <= 2) return 1;
+
+//   return fibonacci(number-1) + fibonacci(number-2);
+// }
+
 
 // Do not edit below this line
 module.exports = fibonacci;
